c73e101c90405bb19272eb04651bcdf9bf1f6130
Britto Sam Jose J <160570250+brittosamjose2004@users.noreply.github.com>
2024-05-11 18:51:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the pre-trained model
-with open('models\logistic_regression_model.pkl', 'rb') as f:
+with open('logistic_regression_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
